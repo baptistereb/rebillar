adc4ecf0b857fa35cd389ddd416563223230a3b5
Baptiste <baptiste.rebillard@outlook.fr>
2022-10-12 16:14:13 +0200

    et voila

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <?php
+$file = fopen('.env', 'rb');
+$env = fread($file, filesize('.env'));
+fclose($file);
 
-//CAS
-require_once("phpCAS-1.3.6/CAS.php");
-phpCAS::client(CAS_VERSION_2_0, "cas.insa-toulouse.fr", 443, 'cas', true);
-phpCAS::setNoCasServerValidation();
-phpCAS::forceAuthentication();
-if (isset($_REQUEST['logout'])) {
-        phpCAS::logout();
+if($env == "LOCAL") {
+	echo "Tu est en LOCAAAAL<br><br>";
+} else {
+	//on importe le CAS
+	require_once("phpCAS-1.3.6/CAS.php");
+	phpCAS::client(CAS_VERSION_2_0, "cas.insa-toulouse.fr", 443, 'cas', true);
+	phpCAS::setNoCasServerValidation();
+	phpCAS::forceAuthentication();
+	if (isset($_REQUEST['logout'])) {
+		phpCAS::logout();
+	}
 }
 
+$now = strtotime("now");
+
+$first_day = mktime(0, 0, 0, 9, 12, 2022); //$hour, $minute, $second, $month, $day, $year
+$last_day = mktime(0, 0, 0, 6, 10, 2023); //$hour, $minute, $second, $month, $day, $year
+
+$percent = 100*($now-$first_day)/($last_day-$first_day);
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,24 +29,50 @@ if (isset($_REQUEST['logout'])) {
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Test</title>
+	<style>
+		.bar {
+			width: 30em;
+			height: 1em;
+			border-radius: 0.5em;
+			position: relative;
+			background: #f2f2f2;
+			box-shadow: inset 0 0 0 1px rgba(0,0,0,0.1);
+		}
+		.bar .percentage {
+		    position: relative;
+		    font-size: 1em;
+		    background: tomato;
+		    height: 1em;
+		    border-radius: 0.5em;
+		  }
+	</style>
 </head>
 <body>
-	Cette page pour le moment ne sert qu'a faire des tests 
+	<main>
 
-	<a href="?logout=">Se déconnecter</a><br><br>
-	<?php var_dump(phpCAS::getAttributes()); ?><br><br><br>
+		Début d'année : <b><?= date("d/m/Y", $first_day) ?></b><br>
+		Date actuelle : <b><?= date("d/m/Y", $now) ?></b><br>
+		Fin d'année : <b><?= date("d/m/Y", $last_day) ?></b><br>
+		Vous en êtes à <b><?= round($percent, 2) ?>%</b> de la MIC, félicitation !<br><br>
+
+		<div class="bar">
+			<div class="percentage has-tip"  style="width: <?= $percent ?>%" data-perc="50%"></div>
+		</div>
+
+		<br><br>
+
+		<a href="?logout=">Se déconnecter</a><br><br>
+	</main>
+	
 	<?php
-	$memberOfCN = phpCAS::getAttributes()["memberOfCN"];
+	//var_dump(phpCAS::getAttributes()); 
+	/*$memberOfCN = phpCAS::getAttributes()["memberOfCN"];
 
 	foreach($memberOfCN as $i) {
 		if(strpos($i, "sve_2_mic") !== FALSE) {
 			echo "<b><font color='red'>Tu est en 2MIC : ".$i."</font><br></b>";
-		} elseif(strpos($i, "sve") !== FALSE) {
-			echo "<b><font color='red'>".$i."</font></b>";
-		} else {
-			echo "<font color='red'>".$i."</font><br>";
 		}
-	}
+	}*/
 	?><br>
 </body>
 </html>
